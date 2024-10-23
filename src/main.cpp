4b7d318e703b350dfba8aa9b92fd985661bbcc34
Witty-Wizard <shashankmarch27@gmail.com>
2024-10-24 02:24:16 +0530

    Malloc bad, static memory allocation good

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,19 @@
 #include <freertos/queue.h>
 
 void setup() {
-  pkt = createPacket(0x01, 8);
-  udpQueue = xQueueCreate(10, sizeof(pkt));
   Serial.begin(115200);
+  udpQueue = xQueueCreate(10, sizeof(pkt));
   startWiFi();
   startmDNS();
   startJoystick(MDNS.IP(0));
-  startAsyncUDPServer(MDNS.IP(0), MDNS.port(0));
+  startAsyncUDPServer(MDNS.port(0));
 }
 
 void loop() {
-  if (xQueueReceive(udpQueue, pkt, portMAX_DELAY)) {
-    Serial.println(pkt->header);
+  if (xQueueReceive(udpQueue, &pkt, portMAX_DELAY)) {
+    Serial.print(pkt.channel[0]);
+    Serial.print("\t");
+    Serial.print(pkt.channel[1]);
+    Serial.print("\n");
   }
 }
